937c91961242069553c7eb3dc5ce7f753f965b78
dennisppaul <dennis.paul@hfk-bremen.de>
2025-07-15 17:03:22 +0200

    added pixels example

---
+++ Basics/pixels/application.cpp
@@ -0,0 +1,29 @@
+#include "Umfeld.h"
+
+using namespace umfeld;
+
+PImage* myImage;
+
+void settings() {
+    size(1024, 768);
+    render_to_buffer = true;
+}
+
+void setup() {
+    myImage = loadImage("drip.png");
+}
+
+void draw() {
+    const int halfImage = width * height / 2;
+    image(myImage, 0, 0);
+
+    loadPixels();
+    for (int i = 0; i < halfImage; i++) {
+        pixels[i + halfImage] = pixels[i];
+    }
+    updatePixels();
+
+    noStroke();
+    fill(1.0f);
+    debug_text("FPS: " + to_string(frameRate), 10, 20);
+}
