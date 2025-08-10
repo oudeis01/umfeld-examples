cbd566ec3e85f82cae0131089486280a804f42f8
Dennis P Paul <dennis.paul@hfk-bremen.de>
2025-08-10 17:14:02 +0200

    added profiling example

---
+++ Research/profiling-opengl-pipeline/application.cpp
@@ -0,0 +1,154 @@
+#include "Umfeld.h"
+#include "Geometry.h"
+
+using namespace umfeld;
+
+int   stroke_join_mode = MITER;
+int   stroke_cap_mode  = ROUND;
+float stroke_weight    = 30;
+bool  close_shape      = false;
+
+void settings() {
+    size(1024, 768);
+}
+
+void setup() {
+    strokeJoin(stroke_join_mode);
+    strokeCap(stroke_cap_mode);
+    strokeWeight(stroke_weight);
+
+    hint(ENABLE_SMOOTH_LINES);
+    g->stroke_properties(radians(10), radians(10), 179);
+    g->set_stroke_render_mode(STROKE_RENDER_MODE_TRIANGULATE_2D);
+}
+
+void draw() {
+    TRACE_FRAME;
+    background(0.85f);
+
+    {
+        TRACE_SCOPE_N("CIRCLE_STROKE_FILL");
+        strokeWeight(15);
+        stroke(0.0f);
+        fill(0.5f, 0.85f, 1.0f);
+        circle(width / 2.0f, height / 2, mouseY);
+        strokeWeight(stroke_weight);
+    }
+    {
+        TRACE_SCOPE_N("CIRCLE_FILL");
+        noStroke();
+        fill(0.5f, 0.85f, 1.0f);
+        circle(width / 2.0f, height / 2, mouseY - 30);
+    }
+    {
+        TRACE_SCOPE_N("CIRCLE_STROKE");
+        strokeWeight(15);
+        stroke(0.0f);
+        noFill();
+        circle(width / 2.0f, height / 2, mouseY - 60);
+        strokeWeight(stroke_weight);
+    }
+    {
+        TRACE_SCOPE_N("POLYGON");
+        stroke(0.0f);
+        fill(0.5f, 0.85f, 1.0f);
+        beginShape(POLYGON);
+        vertex(412, 204);
+        vertex(522, 204);
+        vertex(mouseX, mouseY);
+        vertex(632, 314);
+        vertex(632, 424);
+        vertex(412, 424);
+        vertex(312, 314);
+        endShape(close_shape);
+    }
+    {
+        TRACE_SCOPE_N("LINE");
+        noFill();
+        stroke(1.0f, 0.25f, 0.35f);
+        line(width / 2.0f - 30, height / 2 - 100, width / 2.0f + 30, height / 2 - 40);
+    }
+}
+
+void keyPressed() {
+    if (key == '-') {
+        stroke_weight -= 0.25f;
+        if (stroke_weight < 0) { stroke_weight = 0; }
+        strokeWeight(stroke_weight);
+        console("stroke_weight: ", stroke_weight);
+    }
+    if (key == '+') {
+        stroke_weight += 0.25f;
+        strokeWeight(stroke_weight);
+        console("stroke_weight: ", stroke_weight);
+    }
+    if (key == '1') {
+        stroke_join_mode = NONE;
+        strokeJoin(NONE);
+        console("NONE");
+    }
+    if (key == '2') {
+        stroke_join_mode = BEVEL;
+        strokeJoin(BEVEL);
+        console("BEVEL");
+    }
+    if (key == '3') {
+        stroke_join_mode = MITER;
+        strokeJoin(MITER);
+        console("MITER");
+    }
+    if (key == '4') {
+        stroke_join_mode = ROUND;
+        strokeJoin(ROUND);
+        console("ROUND");
+    }
+    if (key == '5') {
+        stroke_join_mode = MITER_FAST;
+        strokeJoin(MITER_FAST);
+        console("MITER_FAST");
+    }
+    if (key == '6') {
+        stroke_join_mode = BEVEL_FAST;
+        strokeJoin(BEVEL_FAST);
+        console("BEVEL_FAST");
+    }
+    if (key == 'q') {
+        stroke_cap_mode = POINTED;
+        strokeCap(POINTED);
+        console("POINTED");
+    }
+    if (key == 'w') {
+        stroke_cap_mode = PROJECT;
+        strokeCap(PROJECT);
+        console("PROJECT");
+    }
+    if (key == 'e') {
+        stroke_cap_mode = ROUND;
+        strokeCap(ROUND);
+        console("ROUND");
+    }
+    if (key == 'r') {
+        stroke_cap_mode = SQUARE;
+        strokeCap(SQUARE);
+        console("SQUARE");
+    }
+    if (key == 'a') {
+        g->set_stroke_render_mode(STROKE_RENDER_MODE_NATIVE);
+        console("STROKE_RENDER_MODE_NATIVE");
+    }
+    if (key == 's') {
+        g->set_stroke_render_mode(STROKE_RENDER_MODE_TRIANGULATE_2D);
+        console("STROKE_RENDER_MODE_TRIANGULATE_2D");
+    }
+    if (key == 'd') {
+        g->set_stroke_render_mode(STROKE_RENDER_MODE_TUBE_3D);
+        console("STROKE_RENDER_MODE_TUBE_3D");
+    }
+    if (key == 'f') {
+        g->set_stroke_render_mode(STROKE_RENDER_MODE_LINE_SHADER);
+        console("STROKE_RENDER_MODE_LINE_SHADER");
+    }
+    if (key == ' ') {
+        close_shape = !close_shape;
+    }
+}
